e3c6725b3f160c026111e09943847ea12978954c
luis15pt <100942871+luis15pt@users.noreply.github.com>
2022-03-30 12:30:14 +0100

    Update linkspeed.sh

--- linkspeed.sh
+++ linkspeed.sh
@@ -5,4 +5,9 @@ Link8x=$(nvidia-info | grep "8x" -c)
 echo "GPUs at 4x speed:" $Link4x
 echo "GPUs at 8x speed:" $Link8x
 
-message info "GPUs at 4x speed:"$Link4x
+if ($Link8x < 6 )
+then
+message danger "GPUs at 4x speed:"$Link4x
+else
+message info "GPUs at 8x speed:"$Link8x
+fi
