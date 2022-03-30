7d8d7ed74ef49eb71503f0e00e2d3b8e37c6720f
luis15pt <100942871+luis15pt@users.noreply.github.com>
2022-03-30 13:59:24 +0100

    Update linkspeed.sh

--- linkspeed.sh
+++ linkspeed.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+Total=$(nvidia-info | grep "Bios" -c)
 Link4x=$(nvidia-info | grep "4x" -c)
 Link8x=$(nvidia-info | grep "8x" -c)
 
 echo "GPUs at 4x speed:" $Link4x
 echo "GPUs at 8x speed:" $Link8x
 
-if (($Link8x < 6 ))
+if (($Link8x < $Total ))
 then
-message danger "GPUs at 4x speed:"$Link4x
+message danger "GPUs at 4x speed: "$Link4x
+
 else
-message info "GPUs at 8x speed:"$Link8x
+message info "All at 8x Width"$
 fi
