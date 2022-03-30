c8c6593f83b049a93a83a320c4ec9cb4452f05d4
luis15pt <100942871+luis15pt@users.noreply.github.com>
2022-03-30 14:51:30 +0100

    Update linkspeed.sh

--- linkspeed.sh
+++ linkspeed.sh
@@ -3,7 +3,7 @@ Total=$(nvidia-info | grep "Bios" -c)
 Link4x=$(nvidia-info | grep "4x" -c)
 Link8x=$(nvidia-info | grep "8x" -c)
 
-nvidia-info | grep -B 1 "4x" | grep "GPU" | awk '{print $2,$3,$4}' > /tmp/Gpulist.txt
+nvidia-info | grep -B 1 "4x" | grep "GPU" > /tmp/Gpulist.txt
 
 echo "GPUs at 4x speed:" $Link4x
 echo "GPUs at 8x speed:" $Link8x
