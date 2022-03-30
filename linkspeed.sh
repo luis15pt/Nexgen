9477129d83d3f2077f040d0a9f1c505530b15c06
luis15pt <100942871+luis15pt@users.noreply.github.com>
2022-03-30 14:42:28 +0100

    Update linkspeed.sh

--- linkspeed.sh
+++ linkspeed.sh
@@ -5,13 +5,13 @@ Link8x=$(nvidia-info | grep "8x" -c)
 
 nvidia-info | grep -B 1 "4x" | grep "GPU" | awk '{print $2,$3,$4}' > /tmp/Gpulist.txt
 
-#echo "GPUs at 4x speed:" $Link4x
-#echo "GPUs at 8x speed:" $Link8x
+echo "GPUs at 4x speed:" $Link4x
+echo "GPUs at 8x speed:" $Link8x
 
 if (($Link8x < $Total ))
 then
 message danger "GPUs at 4x speed: "$Link4x
-message file List /tmp/Gpulist.txt
+message file List /tmp/Gpulist.txt payload
 else
 message info "All at 8x Width"$
 fi
