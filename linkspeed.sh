03f77c5830a904029d992272a61bb798f681bcd6
luis15pt <100942871+luis15pt@users.noreply.github.com>
2022-03-30 15:44:07 +0100

    Update linkspeed.sh

--- linkspeed.sh
+++ linkspeed.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 Total=$(nvidia-info | grep "Bios" -c)
+Link1x=$(nvidia-info | grep "1x" -c)
 Link4x=$(nvidia-info | grep "4x" -c)
 Link8x=$(nvidia-info | grep "8x" -c)
+Link16x=$(nvidia-info | grep "16x" -c)
+
+LinkTotal=$(($Link1x + $Link4))
 
 nvidia-smi -q | grep --color=never -A 2 -B 6 "Link Width" > /tmp/Gpulist.txt
 
-echo "GPUs at 4x speed:" $Link4x
-echo "GPUs at 8x speed:" $Link8x
+
 
 if (($Link8x < $Total ))
 then
 message info "Total GPUS found: "$Total
-message danger "GPUs at 4x speed: "$Link4x
+message danger "GPUs with issues: "$LinkTotal
 message file GPUList.txt --file=/tmp/Gpulist.txt payload
 else
 message info "All at 8x Width"
