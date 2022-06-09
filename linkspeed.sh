252646529d2021352658333c2016457001e4b669
luis15pt <100942871+luis15pt@users.noreply.github.com>
2022-06-09 09:32:18 +0100

    Update linkspeed.sh

--- linkspeed.sh
+++ linkspeed.sh
@@ -4,6 +4,7 @@ Link1x=$(nvidia-info | grep "1x" -c)
 Link4x=$(nvidia-info | grep "4x" -c)
 Link8x=$(nvidia-info | grep "8x" -c)
 Link16x=$(nvidia-info | grep "16x" -c)
+Nvlinks=$(nvidia-smi nvlink -s | grep "Link" -c)
 
 LinkTotal=$(expr $Link1x + $Link4x)
 echo $LinkTotal
@@ -18,4 +19,11 @@ message danger "GPUs with issues: "$LinkTotal
 message file GPUList.txt --file=/tmp/Gpulist.txt payload
 else
 message info "All at 8x Width"
+fi
+
+if (($Nvlinks < 32 ))
+then
+message danger "Nvlink issues found"
+else
+message info "All NVLinks running"
 fi
